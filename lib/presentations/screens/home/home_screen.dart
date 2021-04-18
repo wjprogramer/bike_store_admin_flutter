df7c0ce2ce760b9017a7eb42449e0593b46d37e5
jay_wu <jay_wu>
2021-04-18 14:29:48 +0800

    use mixin to setup route view model

--- lib/presentations/screens/home/home_screen.dart
+++ lib/presentations/screens/home/home_screen.dart
@@ -1,21 +1,16 @@
+import 'package:bike_store_admin_flutter/core/utils/mixins/routing.dart';
 import 'package:bike_store_admin_flutter/navigation/router_delegate.dart';
-import 'package:bike_store_admin_flutter/presentations/view_models/router_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  RouterViewModel _routerViewModel;
-
+class _HomeScreenState extends State<HomeScreen> with Routing {
   @override
   Widget build(BuildContext context) {
-    final delegate = Router.of(context).routerDelegate as MyRouterDelegate;
-    _routerViewModel = context.watch();
-    _routerViewModel.setRouterDelegate(delegate);
+    setupRoute(context);
 
     return Container(
       color: Colors.green[100],
--- lib/presentations/screens/product_list/product_list_screen.dart
+++ lib/presentations/screens/product_list/product_list_screen.dart
@@ -1,22 +1,14 @@
-import 'package:bike_store_admin_flutter/navigation/router_delegate.dart';
-import 'package:bike_store_admin_flutter/presentations/view_models/router_view_model.dart';
+import 'package:bike_store_admin_flutter/core/utils/mixins/routing.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ProductListScreen extends StatefulWidget {
   @override
   _ProductListScreenState createState() => _ProductListScreenState();
 }
 
-class _ProductListScreenState extends State<ProductListScreen> {
-  RouterViewModel _routerViewModel;
-
+class _ProductListScreenState extends State<ProductListScreen> with Routing {
   @override
   Widget build(BuildContext context) {
-    final delegate = Router.of(context).routerDelegate as MyRouterDelegate;
-    _routerViewModel = context.watch();
-    _routerViewModel.setRouterDelegate(delegate);
-
     return Scaffold(
       body: Center(
         child: Text('Product List'),
